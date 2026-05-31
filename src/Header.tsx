a404413b52cb211fd3d0198cc0d5e2b15ced00ea
HondaWet <97485833+HondaWet@users.noreply.github.com>
2026-05-31 17:16:27 -0500

    Removed old name

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,14 +2,17 @@ import type { ReactNode } from 'react';
 import './Header.css'
 
 function Header(): ReactNode {
-  const title = "Teeter"
+  const title = "Balancr"
   return (
     <header id='header'>
       <nav>
         <button onClick={() => window.location.href = '/'}>Home</button>
         <button onClick={() => window.location.href = '/config'}>Config</button>
       </nav>
-      <h1>{title}</h1>
+      <h1>
+        <img src="favicon.svg" alt={title + ' Icon'} />
+        <span>{title}</span>
+      </h1>
     </header>
   );
 }
