b01a6aae7ed9085f07235843dc2bed40d93a59cd
HondaWet <97485833+HondaWet@users.noreply.github.com>
2026-06-04 18:04:40 -0500

    Made the Header icon a button to install the PWA

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,25 @@
 import type { ReactNode } from 'react';
 import './Header.css'
 
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+    outcome: 'accepted' | 'dismissed';
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
+
+let deferredEvent: BeforeInstallPromptEvent;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // prevent the browser from displaying the default install dialog
+  e.preventDefault();
+
+  // Stash the event so it can be triggered later when the user clicks the button
+  deferredEvent = e as BeforeInstallPromptEvent;
+});
+
 function Header(): ReactNode {
   const title = "Balancr"
   return (
@@ -10,7 +29,11 @@ function Header(): ReactNode {
         <button onClick={() => window.location.href = '/config'}>Config</button>
       </nav>
       <h1>
-        <img src="/favicon.svg" alt={title + ' Icon'} />
+        <img
+          style={{ cursor: "pointer" }}
+          src="/favicon.svg"
+          alt={title + ' Icon'}
+          onClick={() => { if (deferredEvent) deferredEvent.prompt() }} />
         <span>{title}</span>
       </h1>
     </header>
